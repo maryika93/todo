fd937cd9c4a55ff486246fc1a32871cc479ff8b8
Maria <maryika93@mail.ru>
2018-03-11 18:45:08 +0600

    todo

--- index.php
+++ index.php
@@ -1,18 +1,49 @@
 <?php
-$filename = 'dump.txt'; // Имя файла дампа
-$mysql_host = 'localhost'; // Адрес сервера MySQL
-$mysql_username = 'mtipikina'; // Имя пользователя MySQL
-$mysql_password = 'neto1539'; // Пароль MySQL
-$mysql_database = 'mtipikina'; // Имя БД
+$servername = "localhost";
+$username = "mtipikina";
+$password = "neto1539";
+$dbname = "mtipikina";
+$q = 'dump.txt';
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
 
-$connection = mysqli_connect($mysql_host, $mysql_username, $mysql_password) or die('Error connecting to MySQL server: ' . mysqli_error());
-mysqli_select_db($connection, $mysql_database) or die('Error selecting MySQL database: ' . mysqli_error());
+    $sql = $conn->exec("CREATE TABLE 'tasks' (
+'id' int(11) NOT NULL AUTO_INCREMENT,
+'description' text NOT NULL,
+'is_done' tinyint(4) NOT NULL DEFAULT '0',
+'date_added' datetime NOT NULL,
+PRIMARY KEY ('id')
+)");
 
-$templine = '';
-$lines = file($filename);
-foreach ($lines as $line){
-    $templine .= $line;
+    if ($_POST) {
+
+        $data = $conn->prepare('INSERT INTO tasks VALUES(:description)');
+        $fieldData = $_POST['newTask'];
+        $data->bindParam(':description', $fieldData);
+        $var = $data->execute();
+
+    }
+
 }
-echo $templine;
+catch(PDOException $e)
+{
+    die("Error: " . $e->getMessage());
+}
+
 ?>
 
+<h1> Список дел на сегодня </h1>
+<form method="post" action="" enctype="multipart/form-data">
+    <input type="text" placeholder="Новая задача" name="newTask">
+    <input type="submit" value="Добавить"><br/><br/>
+</form>
+
+<table border="1", cellpadding="10", width="100%">
+    <tr>
+        <td align="center"> Описание задачи </td>
+        <td align="center"> Дата добавления </td>
+        <td align="center"> Статус </td>
+        <td align="center">  </td>
+    </tr>
+
+</table>
