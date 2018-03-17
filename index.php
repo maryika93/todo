93de722a2c0329e8f6ebdfeacec5a7a0b7bf0af7
Maria <maryika93@mail.ru>
2018-03-17 23:58:16 +0600

    td

--- index.php
+++ index.php
@@ -8,19 +8,34 @@ try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
 
     if(isset($_POST['newTask'])){
+        $data = $conn->prepare('INSERT INTO `tasks`(`description`, `is_done`, `date_added`) VALUES (:descr, :done, :datead)');
+        $data->bindParam(':descr', $fieldData);
+        $data->bindParam(':done', $isdone);
+        $data->bindParam(':datead', date("y.m.d.H:i:s"));
         $fieldData = $_POST['newTask'];
         $isdone = "В процессе";
-        $data = $conn->prepare('INSERT INTO `tasks`(`description`, `is_done`, `date_added`, `do`) VALUES ($fieldData, $isdone, date("y.m.d.H:i:s"), do)');
-        $data->bindParam(':description', $fieldData);
         $data->execute();
     }
+    if(isset($_GET['delete'])){
+        $del = $_GET['delete'];
+        $datadel = $conn->prepare('DELETE FROM `tasks` WHERE id = :id');
+        $datadel->bindParam(':id', $del);
+        $datadel->execute();
+    }
 
+    if(isset($_GET['done'])){
+        $isdone = "Выполнено";
+        $datadone = $conn->prepare('UPDATE `tasks` SET `is_done`=:done WHERE id = :id');
+        $datadone->execute(array(
+            ':done' => $isdone,
+            ':id' => $_GET['done']
+        ));
+    }
 }
 catch(PDOException $e)
 {
     die("Error: " . $e->getMessage());
 }
-
 ?>
 
 <h1> Список дел на сегодня </h1>
@@ -40,15 +55,13 @@ catch(PDOException $e)
     <?php
     $data = $conn->query('SELECT * FROM tasks');
     foreach($data as $rows) {
-        echo '<pre>';
-        print_r($data);
         ?>
             <tr>
-                <td align="center"><?php echo $rows['id'] ?></td>
                 <td align="center"><?php echo $rows['description'] ?></td>
+                <td align="center"><?php echo $rows['date_added'] ?></td>
                 <td align="center"><?php echo $rows['is_done'] ?></td>
-                <td align="center"><?php echo date("y.m.d.H:i:s") ?></td>
-            </tr>
+                <td align="center"><?php echo '<a href="index.php?delete=' . $rows['id'] . '">Удалить</a> <br/>'.'<a href="index.php?done=' . $rows['id'] . '">Выполнить</a>'?></td>
+        </tr>
         <?php
     }
     ?>
